c21a8953894ceb68b7eba8a874849f66f7f960c3
MMohamedRida <mebdoua.m@bessapromotion.com>
2023-08-06 16:01:48 +0100

    Create transactions.ts

---
+++ server/api/models/transactions.ts
@@ -0,0 +1,44 @@
+import { model, Schema } from 'mongoose';
+import { requiredNumber, requiredString } from './helpers/common';
+
+const transactionsSchema = new Schema({
+  orderDate: requiredString,
+  client: {
+    type: Schema.Types.ObjectId,
+    refPath: 'clientModel',
+  },
+  clientModel: {
+    type: String,
+    required: true,
+    enum: ['Supplier', 'Client']
+  },
+  addedAmount: {
+    type: Number,
+    required: true,
+  },
+  oldFunds: {
+    type: Number,
+    required: true,
+  },
+  newFunds: {
+    type: Number,
+    required: true,
+  },
+  orderId: {
+    type: Schema.Types.ObjectId,
+    refPath: 'orderModel',
+  },
+  orderModel: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    enum: ['Payment', 'Receipt', 'Sale']
+  },
+  transactionDate: Date,
+  type: {
+    ...requiredString,
+    enum: ['SALE', 'RECEIPT', 'FUND'],
+  },
+}, { timestamps: true });
+
+const Transaction = model('Transaction', transactionsSchema);
+export default Transaction;
