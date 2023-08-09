5cce1967aeed3b7a30b3989f27ffae517af37775
MMohamedRida <mebdoua.m@bessapromotion.com>
2023-08-09 15:49:17 +0100

    Update Layout.tsx

--- client/src/shared/components/PDF/Layout.tsx
+++ client/src/shared/components/PDF/Layout.tsx
@@ -1,28 +1,20 @@
 import React from 'react';
 import { Page, Document, PDFViewer } from '@react-pdf/renderer';
-// import PropTypes from 'prop-types';
 
 import styles from './styles';
-import PageHeader from './Parts/Header';
-import PageFooter from './Parts/Footer';
+// import PageHeader from './Parts/Header';
+// import PageFooter from './Parts/Footer';
 
 const PageLayout = ({ children }) => (
   <PDFViewer style={{ width: '100%', height: '100%' }}>
     <Document>
       <Page size="A4" style={styles.page}>
-        <PageHeader />
+        {/* <PageHeader /> */}
         {children}
-        <PageFooter />
+        {/* <PageFooter /> */}
       </Page>
     </Document>
   </PDFViewer>
 );
-
-// PageLayout.propTypes = {
-//   children: PropTypes.oneOfType([
-//     PropTypes.node,
-//     PropTypes.arrayOf(PropTypes.node),
-//   ]).isRequired,
-// };
 
 export default PageLayout;
