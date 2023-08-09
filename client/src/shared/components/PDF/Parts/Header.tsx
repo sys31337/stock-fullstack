bc18f237f2eb5408f5f1bc325afc076a9b1deae8
MMohamedRida <mebdoua.m@bessapromotion.com>
2023-08-09 15:49:26 +0100

    Update Header.tsx

--- client/src/shared/components/PDF/Parts/Header.tsx
+++ client/src/shared/components/PDF/Parts/Header.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import { View, Image, Text } from '@react-pdf/renderer';
-import { format } from 'date-fns';
+// import { View, Image, Text } from '@react-pdf/renderer';
+// import { format } from 'date-fns';
 
-import styles from '../styles';
+// import styles from '../styles';
 
 const PageHeader = () => (
-  <View style={styles.pageHeader}>
-    <View>
-      <Image src="/img/logo_bpi.png" style={styles.logo} />
-    </View>
-    <View>
-      <Text style={styles.paragraphTxt}>{`Fait le ${format(new Date(), 'dd/MM/yyyy')}`}</Text>
-    </View>
-  </View>
+  // <View style={styles.pageHeader}>
+  //   <View>
+  //     <Image src="https://olaspagym.com/wp-content/uploads/2022/10/logo-colored.svg" style={styles.logo} />
+  //   </View>
+  //   <View>
+  //     <Text style={styles.paragraphTxt}>{`Fait le ${format(new Date(), 'dd/MM/yyyy')}`}</Text>
+  //   </View>
+  // </View>
+  <></>
 );
 export default PageHeader;
