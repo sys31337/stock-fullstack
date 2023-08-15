3b168a916a0223d6d467e5281e53f0035c19a758
MMohamedRida <mebdoua.m@bessapromotion.com>
2023-08-15 15:55:13 +0100

    Update products.ts

--- server/api/functions/products.ts
+++ server/api/functions/products.ts
@@ -6,7 +6,6 @@ export const buyBillProductHandler = (products: IProduct[]) => {
     const query = { productName, barCode },
       update = { $inc: { quantity }, stack, buyPrice, sellPrice_1, sellPrice_2, sellPrice_3, tva, category, customer, ...rest },
       options = { upsert: true, new: true, setDefaultsOnInsert: true };
-      console.log({ quantity, stack, buyPrice, sellPrice_1, sellPrice_2, sellPrice_3, tva, category, customer, productName, barCode, ...rest })
     await Product.findOneAndUpdate(query, update, options);
   })
 }
