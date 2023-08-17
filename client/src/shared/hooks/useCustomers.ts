17755ade4019a68fe7529c4194b28fb8e4d28ee8
MMohamedRida <mebdoua.m@bessapromotion.com>
2023-08-17 17:01:21 +0100

    Update useCustomers.ts

--- client/src/shared/hooks/useCustomers.ts
+++ client/src/shared/hooks/useCustomers.ts
@@ -3,13 +3,18 @@ import queryClient from "@shared/services/queryClient";
 import { Payload } from "@shared/types/payload";
 import { useMutation, useQuery } from "@tanstack/react-query";
 
+const defaultId = '0a0aaa0a0aa00000aaaaaa0a';
 const useGetAllCustomers = () => useQuery(
   ['Get all customers'],
   async () => axiosInstance
     .request({
       url: 'customers',
     })
-    .then(({ data }) => data),
+    .then(({ data }) => {
+      const defaultCustomer = data.find((customer) => customer._id === defaultId);
+      const restOfCustomers = data.filter((customer) => customer._id !== defaultId);
+      return [defaultCustomer, ...restOfCustomers];
+    }),
 );
 
 const useGetClients = () => useQuery(
