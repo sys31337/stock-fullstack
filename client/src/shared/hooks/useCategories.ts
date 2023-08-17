1280fee68a1e5b6981d83ab4506e628117784a9a
MMohamedRida <mebdoua.m@bessapromotion.com>
2023-08-17 17:01:19 +0100

    Update useCategories.ts

--- client/src/shared/hooks/useCategories.ts
+++ client/src/shared/hooks/useCategories.ts
@@ -2,6 +2,7 @@ import axiosInstance from "@shared/services/api";
 import queryClient from "@shared/services/queryClient";
 import { Payload } from "@shared/types/payload";
 import { useMutation, useQuery } from "@tanstack/react-query";
+const defaultId = '0a0aaa0a0aa00000aaaaaa0a';
 
 const useGetAllCategories = () => useQuery(
   ['Get all categories'],
@@ -9,7 +10,11 @@ const useGetAllCategories = () => useQuery(
     .request({
       url: 'categories',
     })
-    .then(({ data }) => data),
+    .then(({ data }) => {
+      const defaultCategory = data.find((category) => category._id === defaultId);
+      const restOfCategorys = data.filter((category) => category._id !== defaultId);
+      return [defaultCategory, ...restOfCategorys];
+    }),
 );
 
 const useCreateCategory = () => useMutation((payload: Payload) => axiosInstance.request({
