8b21b2f17483f3463b3855b90ddb15efe12e38da
sys31337 <paytf@outlook.com>
2026-08-14 19:45:40 +0100

    Update syncV2.ts

--- src/main/api/routes/v1/helpers/syncV2.ts
+++ src/main/api/routes/v1/helpers/syncV2.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { pushV2, pullV2 } from '@api/controllers/syncV2';
+import { pushV2, pullV2, diagnosticsV2 } from '@api/controllers/syncV2';
 
 const router = express.Router();
 
 router.post('/push', pushV2);
 router.get('/pull/:collection', pullV2);
+router.get('/diagnostics', diagnosticsV2);
 
 export default router;
